d506a213647196dc1b601d5897e4f419f2bd3397
VicenteMutis <101260079+VicenteMutis@users.noreply.github.com>
2022-04-26 09:06:38 -0400

    definir objetivos...

---
+++ repasogit.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 08:51:44 2022
+
+@author: Vicente Mutis
+"""
+
+git me permite seguir el estado (monitorear los cambios) de una carpeta 
+funciona como dropbox o drive. Drive se modifica todo el rato, git se modifica 
+con cada commit que yo haga. 
+
+primer paso, inicializar (git init .)
+antes la rama se llamaba master, (maestro y esclavo xd), ahora se llama main. 
+
+¿Dónde hago git status en anaconda? #En el terminal de windows!! 
+
+#Introducción
+
+#Desarrollo
+
+#Conclusión 
+
+
